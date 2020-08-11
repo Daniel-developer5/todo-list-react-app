e438402f646476e371a06e565242a8a218aa6ea1
Daniel-developer5 <daniel.mudrak2005@gmail.com>
2020-08-11 17:30:20 +0300

    Updates

---
+++ precache-manifest.abc71ed8e157d4c8e6a6b0bce05eb21d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec662a83fa5439c3b9289965590c7a10",
+    "url": "/todo-list-react-app/index.html"
+  },
+  {
+    "revision": "2cda550205a65a655676",
+    "url": "/todo-list-react-app/static/css/main.06be34c0.chunk.css"
+  },
+  {
+    "revision": "ff4b9ef53388dc2326bb",
+    "url": "/todo-list-react-app/static/js/2.fffdd633.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list-react-app/static/js/2.fffdd633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cda550205a65a655676",
+    "url": "/todo-list-react-app/static/js/main.6dbbc7eb.chunk.js"
+  },
+  {
+    "revision": "8b6a03380853df9d4118",
+    "url": "/todo-list-react-app/static/js/runtime-main.82e85330.js"
+  }
+]);
